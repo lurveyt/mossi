2feab27be3fce13351f88ca1996bd64b88fb7770
tim <lurveyt@uw.edu>
2021-03-15 16:56:42 -0700

    for paths in roster_HTML_to_txt.py

--- _code/roster_HTML_to_txt.py
+++ _code/roster_HTML_to_txt.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup
 # files provided by Rich
 # TODO: get 'year' out of the code
 year = '76'
-roster_path = r"..\mossi_stat\rosters"
+roster_path = os.path.join(os.getcwd(), r"mossi_data\rosters")
 
 
 def file_writer(wfile, d):
