69ba16163c8d6e581491842b023719bae99ae05b
tim <lurveyt@uw.edu>
2022-08-07 10:50:13 -0700

    1985 roster

--- _code/roster_HTML_to_txt.py
+++ _code/roster_HTML_to_txt.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup
 
 # files provided by Rich
 # TODO: get 'year' out of the code
-year = '83'
+year = '85'
 roster_path = os.path.join(os.getcwd(), r"mossi_data\rosters")
 
 
