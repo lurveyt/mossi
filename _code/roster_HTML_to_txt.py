fcca4d4dd1d0b315a9e2b8e00ee42fd9f818329d
tim <lurveyt@uw.edu>
2024-03-10 21:06:52 -0700

    1994 rosters, pre draft

--- _code/roster_HTML_to_txt.py
+++ _code/roster_HTML_to_txt.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup
 
 # files provided by Rich
 # TODO: get 'year' out of the code
-year = '93'
+year = '94'
 roster_path = os.path.join(os.getcwd(), r"mossi_data\rosters")
 
 
