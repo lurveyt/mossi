ddc3f86dfa1a9570cde489e4457f7f10c6d08d7e
tim <lurveyt@uw.edu>
2025-07-11 22:05:09 -0700

    2001 pre-draft

--- _code/player_pdf_regex.py
+++ _code/player_pdf_regex.py
@@ -9,7 +9,7 @@ remove header lines
 
 import re
 
-year = 2000
+year = 2001
 filex = r"..\mossi_data\draft\{}_draft_raw.txt".format(year)
 
 text_raw = open(filex, 'r').read()
--- _code/roster_HTML_to_txt.py
+++ _code/roster_HTML_to_txt.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup
 
 # files provided by Rich
 # TODO: get 'year' out of the code
-year = '2000'
+year = '2001'
 print(f"Building '{year} rosters...")
 
 roster_path = os.path.join(os.getcwd(), r"mossi_data\rosters")
